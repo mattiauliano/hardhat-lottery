5dbbea2d4118008c800e3d9a6ee162e198fd6058
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-03 09:56:24 +0100

    Add temporary subId

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -7,6 +7,7 @@ const networkConfig = {
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane:
             "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId: "0",
     },
     31337: {
         name: "hardhat",
