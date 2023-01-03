d4a57340076c81f84647ec39a7f0a2f1e244b851
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-03 09:55:39 +0100

    Fund fake subId

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -4,6 +4,8 @@ const {
     networkConfig,
 } = require("../helper-hardhat-config");
 
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEthers("3");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
@@ -22,11 +24,17 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         const transactionReceipt = await transactionResponse.wait(1);
         // Get access to subId through events
         subscriptionId = transactionReceipt.events[0].args.subId;
+        // Fund the subscription, you'd need LINK on a real net
+        await vrfCoordinatorV2Mock.fundSubscription(
+            subscriptionId,
+            VRF_SUB_FUND_AMOUNT
+        );
     } else {
-        vrfCoordinatorV2Address = networkConfig[chainId][vrfCoordinatorV2];
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+        subscriptionId = networkConfig[chainId]["subscriptionId"];
     }
-    const entranceFee = networkConfig[chainId][entranceFee];
-    const gasLane = networkConfig[chainId][gasLane];
+    const entranceFee = networkConfig[chainId]["entranceFee"];
+    const gasLane = networkConfig[chainId]["gasLane"];
     const args = [vrfCoordinatorV2Address, entranceFee, gasLane];
 
     const raffle = await deploy("Raffle", {
