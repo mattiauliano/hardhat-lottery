b9daa9dfa0e38d8cfeed8a5a3cdaa833a74e580f
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-03 10:21:57 +0100

    Fix: gasLane must be a string

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -14,7 +14,8 @@ const networkConfig = {
     31337: {
         name: "hardhat",
         entranceFee: ethers.utils.parseEther("0.01"),
-        gasLane: "",
+        gasLane:
+            "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         callbackGasLimit: "500000",
         interval: "30",
     },
