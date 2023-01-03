89d6c130d41c8ca1fc168c9d160cf12fa1e5b7e1
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-03 08:24:05 +0100

    Add networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,25 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
     solidity: "0.8.17",
     namedAccounts: {
         deployer: {
