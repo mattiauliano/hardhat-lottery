a225d2b02d120caa03d69c5c6ed8abd6ad3f0f5b
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-03 09:38:56 +0100

    Create a fake subId for mock

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -8,18 +8,27 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = network.config.chainId;
-    let vrfCoordinatorV2Address;
+
+    let vrfCoordinatorV2Address, subscriptionId;
 
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract(
             "VRFCoordinatorV2Mock"
         );
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+        // Create a subId using mock
+        const transactionResponse =
+            await vrfCoordinatorV2Mock.createSubscription();
+        const transactionReceipt = await transactionResponse.wait(1);
+        // Get access to subId through events
+        subscriptionId = transactionReceipt.events[0].args.subId;
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId][vrfCoordinatorV2];
     }
+    const entranceFee = networkConfig[chainId][entranceFee];
+    const gasLane = networkConfig[chainId][gasLane];
+    const args = [vrfCoordinatorV2Address, entranceFee, gasLane];
 
-    const args = [];
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
