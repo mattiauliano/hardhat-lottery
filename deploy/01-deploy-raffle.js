9e1b847acf85472421ea054e52fe7a4be3eaed7e
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-03 09:12:37 +0100

    Change coordinator address based on network

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,12 +1,28 @@
 const { network } = require("hardhat");
+const {
+    developmentChains,
+    networkConfig,
+} = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+    let vrfCoordinatorV2Address;
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract(
+            "VRFCoordinatorV2Mock"
+        );
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId][vrfCoordinatorV2];
+    }
+
+    const args = [];
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
