30c77d0900bb05db660682966236858b0179a2ff
Mattia Uliano <mattiauliano308@gmail.com>
2023-01-05 13:06:26 +0100

    Test fulfillRandomWords function

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -157,5 +157,94 @@ const { assert, expect } = require("chai");
               });
           });
 
-          describe();
+          describe("fulfillRandomWords", () => {
+              // Want to have at least one player in the Raffle
+              beforeEach(async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee });
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]);
+                  await network.provider.request({ method: "evm_mine", params: [] });
+              });
+
+              it("can only be called after performUpkeep", async () => {
+                  // Check vrfCoordinatorV2Mock contract
+                  await expect(
+                      vrfCoordinatorV2Mock.fulfillRandomWords(0, raffle.address)
+                  ).to.be.revertedWith("nonexistent request");
+                  await expect(
+                      vrfCoordinatorV2Mock.fulfillRandomWords(1, raffle.address)
+                  ).to.be.revertedWith("nonexistent request");
+              });
+
+              // Too big, should be splitted ---> LEARNING PURPOSE
+              it("picks a winner, resets the lottery, and sends money", async () => {
+                  const additionalEntrants = 3;
+                  const startingAccountIndex = 1; // deployer = 0
+                  const accounts = await ethers.getSigners();
+                  // Connect and enterRaffle for each account
+                  for (
+                      let i = startingAccountIndex;
+                      i < startingAccountIndex + additionalEntrants;
+                      i++
+                  ) {
+                      const accountConnectedRaffle = raffle.connect(accounts[i]);
+                      await accountConnectedRaffle.enterRaffle({ value: raffleEntranceFee });
+                  }
+
+                  const startingTimeStamp = await raffle.getLatestTimeStamp();
+                  /* In a real test net we must wait fulfillRandomWords to finish
+                     to call performUpkeep. In local net we simulate this
+                     behavior using a listener inside a Promise.
+                  */
+                  await new Promise(async (resolve, reject) => {
+                      // listener for an event
+                      raffle.once("WinnerPicked", async () => {
+                          console.log("Found the event!");
+                          // Catch the error if too much time passes
+                          try {
+                              const recentWinner = await raffle.getRecentWinner();
+                              const raffleState = await raffle.getRaffleState();
+                              const endingTimeStamp = await raffle.getLatestTimeStamp();
+                              const numPlayers = await raffle.getNumOfPlayers();
+                              const winnerEndingBalance = await accounts[1].getBalance();
+
+                              assert.equal(numPlayers.toString(), "0");
+                              assert.equal(raffleState.toString(), "0");
+                              assert(endingTimeStamp > startingTimeStamp);
+                              assert.equal(
+                                  // The winner ending balance must be equal
+                                  winnerEndingBalance.toString(),
+                                  // to all the money that everybody else entered in this contract
+                                  winnerStartingBalance.add(
+                                      raffleEntranceFee
+                                          .mul(additionalEntrants)
+                                          .add(raffleEntranceFee)
+                                          .toString()
+                                  )
+                              );
+
+                              // To know who the winner is
+                              console.log(recentWinner);
+                              console.log(accounts[0].address);
+                              console.log(accounts[1].address);
+                              console.log(accounts[2].address);
+                              console.log(accounts[3].address);
+                          } catch (e) {
+                              reject(e);
+                          }
+                          resolve();
+                      });
+
+                      // Fire the event
+                      const tx = await raffle.performUpkeep([]);
+                      const txReceipt = await tx.wait(1);
+                      // From console logs
+                      const winnerStartingBalance = await accounts[1].getBalance();
+                      await vrfCoordinatorV2Mock.fulfillRandomWords(
+                          vrfCoordinatorV2Mock.interface.parseLog(txReceipt.events[0]).args
+                              .requestId,
+                          raffle.address
+                      );
+                  });
+              });
+          });
       });
